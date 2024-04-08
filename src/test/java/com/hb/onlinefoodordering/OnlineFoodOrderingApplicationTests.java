d59c196dd23324c8d044c4ca5cfbbbbbb258ec94
Hoang Bach <bach.nh@smartmind.vn>
2024-04-08 21:41:44 +0700

    init

---
+++ src/test/java/com/hb/onlinefoodordering/OnlineFoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.onlinefoodordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
